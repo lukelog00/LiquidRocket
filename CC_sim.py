fd771bc63688c278b5e069f190326782b1e7efd4
lukelog00 <luke.logan00@gmail.com>
2025-11-18 02:39:54 +1100

    wahhhhhhhh not sure if physics is right but it looks ok!

--- CC_sim.py
+++ CC_sim.py
@@ -45,17 +45,46 @@ OF_targ = 3
 
 # Propellant tank internal dimensions
 ullage = 0.05
-id_tank = 98e-3 # m
-l_tank = 600e-3 # m
+id_tank = 72.2e-3 # m
+l_tank = 300e-3 # m
 A_tank = np.pi * id_tank ** 2 / 4
 v_tank = l_tank*id_tank**2*np.pi/4 # m3
 LD_tank = l_tank/id_tank
+print("Tank L/D:",round(LD_tank,2))
 
 # Propellant initial state
-T_init = 0 # C
-p_init = 35e5 # Pa
+p_init = 30e5 # Pa
 
-ox = ox_fluid.with_state(Input.temperature(T_init),Input.pressure(p_init))
+# Injector
+inj_ox_d = 2.5e-3 # m
+inj_ox_n = 12 #
+inj_ox_A = np.pi * inj_ox_d ** 2 / 4 * inj_ox_n # m2
+inj_ox_Cd = 0.7 #
+
+inj_f_d = 1e-3 # m
+inj_f_n = 3 #
+inj_f_A = np.pi * inj_f_d ** 2 / 4 * inj_f_n # m2
+inj_f_Cd = 0.7 #
+
+# Nozzle
+d_t = 25e-3 # m
+At = np.pi * d_t ** 2 / 4 #100e-6 # m2
+AeAt = 3.5
+
+# Time stepping
+dt = 0.01 # s
+t_end = 20 # s
+t = np.arange(0,t_end+dt,dt) # s
+n = len(t)
+
+# Initialise
+i = 0
+
+# State
+ox = ox_fluid.with_state(Input.quality(ullage),Input.pressure(p_init))
+
+T_init = ox.temperature + 1
+
 fuel = fuel_fluid.with_state(Input.temperature(T_init),Input.pressure(p_init))
 
 rho_ox = ox.density
@@ -72,25 +101,10 @@ v_f_i = v_tank - v_ox_v - v_ox_i
 m_ox_i = rho_ox*v_ox_i
 m_f_i = rho_f*v_f_i
 
-# Injector
-inj_ox_A = 65e-6 # m2
-inj_ox_Cd = 0.7 #
-inj_f_A = 3.14e-6 # m2
-inj_f_Cd = 0.7 #
+# Ullage mass
+m_ox_v = ullage * m_ox_i
 
-# Nozzle
-At = 2000e-6 # m2
-AeAt = 6
-
-# Time stepping
-dt = 0.01 # s
-t_end = 20 # s
-t = np.arange(0,t_end+dt,dt) # s
-n = len(t)
-
-# Initialise
-i = 0
-
+# Arrays
 p_c = np.full(n,np.nan)
 p_c[0] = p_SSL
 p_s = np.full(n,np.nan)
@@ -105,6 +119,8 @@ OFs = np.full(n,np.nan)
 
 cstars = np.full(n,np.nan)
 dm_noz = np.full(n,np.nan)
+ox_qual = np.full(n,np.nan)
+Isp = np.full(n,np.nan)
 
 Ts = np.full(n,np.nan)
 
@@ -128,10 +144,15 @@ def dm_c(p_c_loc):
 
 for i in range(n-1):
     dP_i = p_s[i] - p_c[i]
-    dm_ox = np.interp(dP_i, dPs_Dyer, dm_Dyer)*dt # Ox mass through inj, kg
-    dm_f = inj_f_Cd * inj_f_A * np.sqrt(2 * rho_f * dP_i)*dt # Fuel mass through inj, kg
+    dm_ox = np.interp(abs(dP_i), dPs_Dyer, dm_Dyer)*dt # Ox mass through inj, kg
+    dm_f = inj_f_Cd * inj_f_A * np.sqrt(2 * rho_f * abs(dP_i))*dt # Fuel mass through inj, kg
+
+    if dP_i < 0:
+        print("Reverse flow detected, this could just mean the burn is finished")
+        break
 
     OFs[i] = dm_ox/dm_f
+    ox_qual[i] = ox.quality
 
     # CEA
     cstars[i] = cea.get_Cstar(Pc=p_c[i]/1e5, MR=OFs[i])
@@ -150,6 +171,10 @@ for i in range(n-1):
     # Thrust
     Cfcea, Cfamb, mode = cea.get_PambCf(Pamb=p_SSL/1e5, Pc=p_c[i]/1e5, MR=OFs[i],eps=AeAt)
     Ts[i] = Cfamb*p_c[i]*At # Thrust at given ambient pressure
+
+    # Isp
+    IspVac = cea.get_Isp(Pc=p_c[i]/1e5, MR=OFs[i],eps=AeAt)
+    Isp[i], mode = cea.estimate_Ambient_Isp(Pamb=p_SSL/1e5, Pc=p_c[i]/1e5, MR=OFs[i],eps=AeAt)
 
     # Update propellant masses
     m_oxs[i+1] = m_oxs[i] - dm_ox
@@ -188,7 +213,12 @@ for i in range(n-1):
 
 ### OUTPUT
 
+impulse = scipy.integrate.trapezoid(Ts[~np.isnan(Ts)],x=t[~np.isnan(Ts)])
+
 print("Nozzle mode:",mode)
+print("Impulse:",round(impulse),"Ns")
+print("Thrust:",round(Ts[3]),"N")
+print("Isp:",round(np.mean(Isp[~np.isnan(Isp)]),1),"s")
 
 ### PLOTTING
 
@@ -200,14 +230,31 @@ plt.ylabel("Mass (kg)")
 plt.legend()
 
 plt.figure()
+plt.plot(t,ox_qual)
+plt.xlabel("Time (s)")
+plt.ylabel("Oxidiser vapor quality (%)")
+
+plt.figure()
 plt.plot(t,Ts)
 plt.xlabel("Time (s)")
 plt.ylabel("Thrust (N)")
 
 plt.figure()
+plt.plot(t,p_s/1e5,label="supply")
+plt.plot(t,p_c/1e5,label="chamber")
+plt.xlabel("Time (s)")
+plt.ylabel("Pressure (bar)")
+plt.legend()
+
+plt.figure()
 plt.plot(t,OFs)
 plt.xlabel("Time (s)")
 plt.ylabel("OF ratio")
+
+# plt.figure()
+# plt.plot(t,Isp)
+# plt.xlabel("Time (s)")
+# plt.ylabel("Isp (s)")
 
 plt.show()
 
